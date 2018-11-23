0d273dcf05bfb9b0cc71456219c0c1174b79027c
sonw <460080137@qq.com>
2018-11-23 17:16:34 +0800

    抢救车-台账-样式调整

--- src/routes/BaseDrug/salvageCar/salvageCarLedger/index.js
+++ src/routes/BaseDrug/salvageCar/salvageCarLedger/index.js
@@ -20,11 +20,11 @@ const { Option } = Select;
 const singleFormItemLayout = {
     labelCol: {
       xs: { span: 24 },
-      sm: { span: 10 },
+      sm: { span: 6 },
     },
     wrapperCol: {
       xs: { span: 24 },
-      sm: { span: 14 },
+      sm: { span: 18 },
     },
   }
   const IndexColumns = [
