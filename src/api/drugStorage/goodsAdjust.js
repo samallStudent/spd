9f0d6e752946f387ef20a1208457e2c670434b9d
xuhao <984446709@qq.com>
2019-01-17 17:21:36 +0800

    【货位移动优化】

--- src/api/drugStorage/goodsAdjust.js
+++ src/api/drugStorage/goodsAdjust.js
@@ -7,4 +7,5 @@ export default {
   goodsList: `${_local}/a/roomlocadjust/list`,      //货位移动列表
   roomDrugList: `${_local}/a/roomlocadjust/roomDrugList`,         //添加产品查询列表
   QUERY_DRUG_BY_LIST: `${_local}/a/common/queryDrugByList`,  //添加产品 - 下拉框
+  QUERY_TARGET_LOCATION: `${_local}/a/roomlocadjust/queryTargetLocation`,   //获得目的货位
 }
